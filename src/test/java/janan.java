aea73396cd8f93917b1eba12a78ec43892d2af85
fahimmohammadi <157770910+fahimmohammadi@users.noreply.github.com>
2024-05-09 20:32:34 -0500

    Revert "Test1"

--- src/test/java/janan.java
+++ src/test/java/janan.java
@@ -1,9 +1,5 @@
 public class janan {
     public static void main (String[]args){
         System.out.println("my name is janan and i am lewany");
-
-        System.out.println("javid is doing hamam");
-
-        System.out.println("my name is three idiot");
     }
 }
