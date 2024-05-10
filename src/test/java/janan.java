f18c3c7bb7c54689ea2fcc2d9d1691ad073159e2
Fahim <fahim.mohammadi@tekschool.us>
2024-05-09 20:10:38 -0500

    part of janan

--- src/test/java/janan.java
+++ src/test/java/janan.java
@@ -1,5 +1,7 @@
 public class janan {
     public static void main (String[]args){
         System.out.println("my name is janan and i am lewany");
+
+        System.out.println("javid is doing hamam");
     }
 }
